76d832df60d74214af217347db1f044d97ab327f
snipher <sniphermarube@gmail.com>
2024-12-03 17:50:46 +0300

    fixing cloudinary for production

--- cloudinary_helper/apps.py
+++ cloudinary_helper/apps.py
@@ -1,9 +1,18 @@
 from django.apps import AppConfig
+from django.conf import settings
+from cloudinary.uploader import config
+from .utils import setup_cloudinary 
+
 
 class CloudinaryHelperConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "cloudinary_helper"
 
     def ready(self):
-        # No need to replace FileField or ImageField classes here.
-        pass
+        if not settings.DEBUG:
+            # Automatically configure Cloudinary in production
+            setup_cloudinary(
+                settings.CLOUDINARY_STORAGE['CLOUD_NAME'],
+                settings.CLOUDINARY_STORAGE['API_KEY'],
+                settings.CLOUDINARY_STORAGE['API_SECRET']
+            )
--- cloudinary_helper/storages.py
+++ cloudinary_helper/storages.py
@@ -1,27 +1,41 @@
 from django.conf import settings
-from django.core.files.storage import FileSystemStorage
+from django.core.files.storage import Storage
 import cloudinary.uploader
+from django.core.files.storage import FileSystemStorage
 from cloudinary import CloudinaryImage
 import os
 
 
-class CloudinaryMediaStorage(FileSystemStorage):
+class CloudinaryMediaStorage(Storage):
     """Custom storage for Cloudinary Media Files."""
     def _save(self, name, content):
+        # Ensure the name is valid for Cloudinary
         name = os.path.basename(name)
         response = cloudinary.uploader.upload(content, public_id=name)
-        return response['public_id']
+        return response["public_id"]
 
     def url(self, name):
         return CloudinaryImage(name).build_url()
 
 
-class CloudinaryStaticStorage(FileSystemStorage):
+class CloudinaryStaticStorage(Storage):
     """Custom storage for Cloudinary Static Files."""
     def _save(self, name, content):
         name = os.path.basename(name)
         response = cloudinary.uploader.upload(content, public_id=name, resource_type="raw")
-        return response['public_id']
+        return response["public_id"]
 
     def url(self, name):
         return CloudinaryImage(name).build_url()
+
+
+def is_production():
+    """Helper function to determine if the environment is production."""
+    return not settings.DEBUG
+
+
+def get_storage_class():
+    """Return the appropriate storage class."""
+    if is_production():
+        return CloudinaryMediaStorage
+    return FileSystemStorage
