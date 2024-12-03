ef5a91b43066aeb5ace4cd2941a7922a75f780b7
snipher <sniphermarube@gmail.com>
2024-12-03 13:47:41 +0300

    Handling fields

--- cloudinary_helper/utils.py
+++ cloudinary_helper/utils.py
@@ -1,5 +1,6 @@
-from django.conf import settings
+# utils.py
 
+from django.conf import settings
 
 def setup_cloudinary(cloud_name, api_key, api_secret):
     # Set up Cloudinary configurations
@@ -10,10 +11,11 @@ def setup_cloudinary(cloud_name, api_key, api_secret):
     }
     settings.DEFAULT_FILE_STORAGE = 'cloudinary_helper.storages.MediaStorage'
     settings.STATICFILES_STORAGE = 'cloudinary_helper.storages.StaticStorage'
-   
-
-
     
-
-
-
+    # Import cloudinary after settings have been defined to avoid circular import
+    import cloudinary
+    cloudinary.config(
+        cloud_name=settings.CLOUDINARY_STORAGE['CLOUD_NAME'],
+        api_key=settings.CLOUDINARY_STORAGE['API_KEY'],
+        api_secret=settings.CLOUDINARY_STORAGE['API_SECRET']
+    )
