74d473c32a4f8eeae750545cbb382c9ef1682f18
snipher <sniphermarube@gmail.com>
2024-12-03 21:10:31 +0300

    code optimization

--- cloudinary_helper/utils.py
+++ cloudinary_helper/utils.py
@@ -8,10 +8,49 @@ def setup_cloudinary(cloud_name, api_key, api_secret):
         api_key=api_key,
         api_secret=api_secret
     )
+    settings.DEFAULT_FILE_STORAGE = 'cloudinary_helper.storages.CloudinaryMediaStorage'
+    settings.STATICFILES_STORAGE = 'cloudinary_helper.storages.CloudinaryStaticStorage'
 
-    
-    # Set the file storage settings to use Cloudinary
-    if not settings.DEFAULT_FILE_STORAGE:
-        settings.DEFAULT_FILE_STORAGE = 'cloudinary_helper.storages.CloudinaryMediaStorage'
-    if not settings.STATICFILES_STORAGE:
-        settings.STATICFILES_STORAGE = 'cloudinary_helper.storages.CloudinaryStaticStorage'
+    DEFAULT_UPLOAD_OPTIONS = {
+        "folder": "default",
+        "overwrite": True,
+        "resource_type": "auto"
+    }
+
+    return DEFAULT_UPLOAD_OPTIONS
+
+def upload_image(image, options=None):
+    if options is None:
+        options = {}
+
+    upload_options = {**options}
+
+    return cloudinary.uploader.upload(image, **upload_options)
+
+def delete_image(public_id):
+    return cloudinary.uploader.destroy(public_id)
+
+def get_image(public_id):
+    return cloudinary.api.resource(public_id)
+
+def get_images():
+    return cloudinary.api.resources()
+
+def get_image_url(public_id):
+    return cloudinary.utils.cloudinary_url(public_id)[0]
+
+def get_image_thumbnail_url(public_id, width, height):
+    return cloudinary.utils.cloudinary_url(public_id, width=width, height=height, crop="fill")[0]
+
+def get_image_responsive_url(public_id, width, height):
+    return cloudinary.utils.cloudinary_url(public_id, width=width, height=height, crop="fill", responsive=True)[0]
+
+def get_image_responsive_srcset(public_id, width, height):
+    return cloudinary.utils.cloudinary_url(public_id, width=width, height=height, crop="fill", responsive=True)[1]
+
+def get_image_responsive_sizes():
+    return "(min-width: 768px) 50vw, 100vw"
+
+def get_image_responsive_image(public_id, width, height):
+    return f'<img src="{get_image_responsive_url(public_id, width, height)}" srcset="{get_image_responsive_srcset(public_id, width, height)}" sizes="{get_image_responsive_sizes()}" alt="{public_id}">'
+
